092106b3582bbf85dae3b6f61033a708cdfbdc85
Thomas W <elshaggy1@gmail.com>
2021-06-06 15:21:06 -0400

    fix styling and layout issues

--- socket_gallows/assets/webpack.config.js
+++ socket_gallows/assets/webpack.config.js
@@ -12,9 +12,7 @@ module.exports = (env, options) => ({
       new OptimizeCSSAssetsPlugin({})
     ]
   },
-  entry: {
-    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
-  },
+  entry: glob.sync('./vendor/**/*.js').concat(['./js/app.js']).concat(['./css/app.css']),
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, '../priv/static/js')
